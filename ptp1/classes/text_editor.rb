000f0eecc44262169df12ea11bd23e56f49f90b5
kikelkik <j.krukenberg@gmx.de>
2019-10-07 15:46:42 +0200

    Teamwork PTP 01

--- ptp1/classes/text_editor.rb
+++ ptp1/classes/text_editor.rb
@@ -1,58 +1,57 @@
+# A class used to read text files
 # Author::Youssef Benlemlih
-# A class used to read text files,
+# Author::Jonas Krukenberg
 class TextEditor
-    # Initialize a new instance of *TextEditor*
-    # @param filename: The path to the text file
-    def initialize(path)
-        @filestream = File.open(path)
-        @content = @filestream.read
-    end
+  # content returns an alphabetically sorted word list where :
+  # * all non-word characters are removed
+  # * upcase characters are replaced by downcase ones
+  attr_reader :content, :word_list
+  # Initialize a new instance of *TextEditor*
+  # @param path: The path to the text file
+  def initialize(path)
+    File.open(path) { |f| @content = f.read }
+    @word_list = get_word_list(@content)
+  end
 
-    # Returns raw the content of the file
-    def get_raw_content
-        @content
+  # Returns an array of each word in @param
+  def get_word_list(content)
+    words = content.split
+    word_list = []
+    # filter the words list
+    words.each do |word|
+      # replace all upcase characters with downcase ones
+      w = word.downcase
+      # remove all non-letters characters
+      w.gsub!(/\W/, '')
+      # remove all whitespaces
+      w.gsub!(/\s+/, '')
+      # only all non-empty word to the filtered list
+      word_list.push(w) unless w.empty?
     end
+    word_list
+  end
 
-    # Returns an alphabetically sorted word list where :
-    # * all non-word characters are removed
-    # * upcase characters are replaced by downcase ones
-    def get_words_list
-        words_list = @content.split
-        filtered_word_list=[]
-        # filter the words list
-        words_list.each do |word|
-            # replace all upcase characters with downcase ones
-            w = word.downcase
-            # remove all non-letters characters
-            w.gsub!(/\W/,'')
-            # remove all whitespaces
-            w.gsub!(/\s+/,'')
-            # only all non-empty word to the filtered list
-            if !w.empty? then
-                filtered_word_list.push(w)          
-            end
-        end
-        # sort the filtered word list alphabetically
-        filtered_word_list.sort!
-        # return the filtered word list
-        filtered_word_list
-    end
+  # Returns the words list, where each word is inverted
+  def reverse_words
+    reversed_word_list = []
+    @word_list.each { |word| reversed_word_list.push(word.reverse) }
+    # return the reversed word list
+    reversed_word_list
+  end
 
-    # Returns the words list, where each word is inverted
-    def get_reversed_words_list
-        reversed_word_list=[]
-        get_words_list.each do |word|
-            reversed_word_list.push(word.reverse)
-        end
-        # return the reversed word list
-        reversed_word_list
-    end
+  # Saves the given wordlist in a file in the given path wit one word per line
+  def to_file(wordlist, filepath)
+    text = wordlist.join("\n")
+    File.open(filepath, 'w') { |f| f.write text }
+  end
 
-    # Saves the given wordlist in a file in the given path wit one word per line
-    def self.save_word_list_to_file(wordlist,filepath)
-        text = wordlist.join("\n");
-        file = File.new(filepath,'w')
-        file.write text
-        file.close
-    end
+  # Returns a Hash where:
+  # * *key* is the word
+  # * *value* is the occurrence of the usage of the word in the words list
+  def word_occurrence
+    # the default value of the occurrence of a word is 0
+    word_count = Hash.new(0)
+    @word_list.each { |word| word_count[word] += 1 }
+    word_count
+  end
 end
--- ptp1/string_main.rb
+++ ptp1/string_main.rb
@@ -1,10 +1,11 @@
 # A dummy script to show off the static methods of the class StringUtil
 # Author::Youssef Benlemlih
+# Author::Jonas Krukenberg
 require_relative('./classes/string_util')
 
 puts('Please enter the initial string:')
-# Get the user input and chop the \n
-input = gets.chop!
+# Get the user input and chomp the \r\n
+input = gets.chomp
 
 puts("\n")
 puts("| The input string:        | '#{input}'")
@@ -12,7 +13,6 @@ puts("| The reversed string:     | '#{StringUtil.reverse(input)}'")
 puts("| All chars uppercase:     | '#{StringUtil.upcase(input)}'")
 puts("| All chars downcase:      | '#{StringUtil.downcase(input)}'")
 puts("| Swap case:               | '#{StringUtil.swapcase(input)}''")
-puts("| Replace vocals by '*':   | '#{StringUtil.censor_small_vocals(input)}'")
-puts("| Double consonants with o:| '#{StringUtil.double_consonants_with_o(input)}'")
+puts("| Replace vocals by '*':   | '#{StringUtil.censor_small_vowels(input)}'")
+puts("| Double consonants with o:| '#{StringUtil.kokify(input)}'")
 puts("\n")
-puts("Note: Y is interpreted as a consonant")
