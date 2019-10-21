77ad2c9655456d58ec3e1f2462ec97cfa62f5db2
youssefbenlemlih <youssefbenlemlih7@gmail.com>
2019-10-21 16:49:12 +0200

    added repeat_wanted in interface

--- ptp3/interface.rb
+++ ptp3/interface.rb
@@ -12,34 +12,42 @@ class Interface
   end
 
   def interact
-    puts "Welchen Wert wollen Sie umrechnen? [Bsp:100km]"
-    first_input = next_input
-    while !valid_first_input?(first_input)
-      puts "ungultige eingabe. Bitte erneut versuchen [Bsp:50mm]"
+    repeat_wanted = true
+    while repeat_wanted
+      puts "Welchen Wert wollen Sie umrechnen? [Bsp:100km]"
       first_input = next_input
-    end
-    puts "Was ist die Zieleinheit du der Sie umrechnen wollen?? [Bsp:mm]"
-    second_input = next_input
-    while !valid_second_input?(second_input)
-      puts "ungultige eingabe. Bitte erneut versuchen [Bsp:cm]"
+      until valid_first_input?(first_input)
+        puts "ungultige eingabe. Bitte erneut versuchen [Bsp:50mm]"
+        first_input = next_input
+      end
+      puts "Was ist die Zieleinheit du der Sie umrechnen wollen?? [Bsp:mm]"
       second_input = next_input
-    end
-    num = (first_input.match(/\d*/)[0]).to_i
-    unit_from = first_input.delete_prefix(num.to_s)
-    unit_to = second_input
-    result = @converter.convert(num,unit_from,unit_to)
-    if result
-      puts "#{first_input} = #{result} #{unit_to}"
-    else
-      puts "Kann nicht von #{unit_from} zu #{unit_to} konvertieren"
+      until valid_second_input?(second_input)
+        puts "ungultige eingabe. Bitte erneut versuchen [Bsp:cm]"
+        second_input = next_input
+      end
+      num = (first_input.match(/\d*/)[0]).to_i
+      unit_from = first_input.delete_prefix(num.to_s)
+      unit_to = second_input
+      result = @converter.convert(num, unit_from, unit_to)
+      if result
+        puts "#{num} #{unit_from} = #{result} #{unit_to}"
+      else
+        puts "Kann nicht von #{unit_from} zu #{unit_to} konvertieren"
+      end
+      puts "Wollen Sie eine andere Konversion machen? [j/ja/nein/n]"
+      input = next_input
+      repeat_wanted = input.downcase == "ja" || input.downcase == "j"
     end
   end
+
   def valid_first_input?(input)
     matches = /^\d+([a-z].*)/m.match(input)
     unit = matches.nil? ? false : matches[1]
     @allowed_units.include?(unit)
   end
+
   def valid_second_input?(input)
-  @allowed_units.include?(input)
+    @allowed_units.include?(input)
   end
 end
