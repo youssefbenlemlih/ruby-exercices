1fa91f67c4e1ac3a15cb14b014beaca3c957c3d2
kikelkik <j.krukenberg@gmx.de>
2019-10-24 22:04:44 +0200

    units_config.json umstrukturiert unnits_configold.json aufgehoben Berechnung mit delta klappt noch nicht

--- ptp3/converter.rb
+++ ptp3/converter.rb
@@ -9,17 +9,8 @@ class Converter
   end
 
   def convert(num, u1, u2)
-    cat = @units_config[unit_category(u1)]
-    case cat['operation']
-    when 'multiply'
-      return num * cat['units'][u1] / cat['units'][u2]
-    when 'formular'
-      return convert_with_formular(cat, num, u1, u2)
-    when 'count'
-      puts 'Anzahl...'
-    else
-      return '1'
-    end
+    units = @units_config[unit_category(u1)]['units']
+    num * (units[u1]['factor'] - units[u1]['delta']) / (units[u2]['factor'] - units[u2]['delta'])
   end
 
   def convert_with_formular(cat, num, u1, u2)
