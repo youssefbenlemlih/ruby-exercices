87cf8f66319ad5aaa826c5d2d46240ec890a5cc8
youssef <youssefbenlemlih7@gmail.com>
2019-10-27 21:10:24 +0100

    use @converter in tests.rb

--- ptp3/interface.rb
+++ ptp3/interface.rb
@@ -2,39 +2,40 @@
 # Author:: Jonas Krukenberg
 # This class manages frontend interaction with the user,
 # validates inputs, outputs results
-require_relative 'converter'
+require_relative "converter"
 
 class Interface
   attr_reader :converter
+
   def initialize
     @converter = Converter.new
     @all_units = @converter.list_units
   end
 
   def next_input
-    print '>> '
+    print ">> "
     gets.chomp.strip.downcase
   end
 
   def interact
     begin
-      puts 'Welchen Wert wollen Sie umrechnen? [Bsp:100km]'
-      puts "Erlaubte Einheiten: #{@all_units.to_s}"
+      puts "Welchen Wert wollen Sie umrechnen? [Bsp:100km]"
+      puts "Erlaubte Einheiten: #{@all_units}"
       first_input = next_input
       until valid_first_input?(first_input)
-        puts 'Ungueltige Eingabe. Bitte erneut versuchen [Bsp:50 mm]'
-        puts "Erlaubte Einheiten: #{@all_units.to_s}"
+        puts "Ungueltige Eingabe. Bitte erneut versuchen [Bsp:50 mm]"
+        puts "Erlaubte Einheiten: #{@all_units}"
         first_input = next_input
       end
       num = (first_input[/[-\d.]+/]).to_f
       unit_from = (first_input[/[a-z]+.*/])
       @converter.category!(unit_from)
 
-      puts 'Was ist die Zieleinheit, zu der Sie umrechnen wollen?'
+      puts "Was ist die Zieleinheit, zu der Sie umrechnen wollen?"
       puts "Erlaubte Einheiten: #{allowed_units}"
       second_input = next_input
       until valid_second_input?(second_input)
-        puts 'Ungueltige Eingabe. Bitte erneut versuchen'
+        puts "Ungueltige Eingabe. Bitte erneut versuchen"
         puts "Erlaubte Einheiten: #{allowed_units}"
         second_input = next_input
       end
@@ -45,7 +46,7 @@ class Interface
       else
         puts "Kann nicht von #{unit_from} zu #{unit_to} konvertieren"
       end
-      puts 'Wollen Sie eine andere Konversion machen? [j/ja/nein/n]'
+      puts "Wollen Sie eine andere Konversion machen? [j/ja/nein/n]"
     end while %w[ja j].include?(next_input)
   end
 
--- ptp3/tests.rb
+++ ptp3/tests.rb
@@ -1,30 +1,33 @@
 # Author:: Youssef Benlemlih
 # Author:: Jonas Krukenberg
 # Class for testing the application
-require 'test/unit'
-require_relative 'interface'
+require "test/unit"
+require_relative "interface"
+require_relative "converter"
 
 class Tests < Test::Unit::TestCase
   def setup
     @interface = Interface.new
+    @converter = Converter.new
+
     @first_input_params = [
-        [true, '100m'],
-        [true, '100km'],
-        [true, '10.03kg'],
-        [true, '100 m'],
-        [false, 'g100m'],
-        [true, '.3m'],
-        [false, 'm']
+      [true, "100m"],
+      [true, "100km"],
+      [true, "10.03kg"],
+      [true, "100 m"],
+      [false, "g100m"],
+      [true, ".3m"],
+      [false, "m"],
     ]
     @second_input_params = [
-        [true, 'kg', 't'],
-        [true, 'mm', 'ft'],
-        [false, 'mm', 'kg'],
-        [false, 'mm', '1'],
-        [false, 'kg', 'g t']
+      [true, "kg", "t"],
+      [true, "mm", "ft"],
+      [false, "mm", "kg"],
+      [false, "mm", "1"],
+      [false, "kg", "g t"],
     ]
 
-    File.open('./test_data/converter_test_data.json') do |file|
+    File.open("./test_data/converter_test_data.json") do |file|
       @converter_data = JSON.parse(file.read.chomp)
     end
   end
@@ -45,12 +48,12 @@ class Tests < Test::Unit::TestCase
       unit1 = e[1]
       value2 = e[2]
       unit2 = e[3]
-      @interface.converter.category!(unit1)
+      @converter.category!(unit1)
       # from value1 to value2
-      assert_equal(value2, @interface.converter.convert(value1, unit1, unit2),
+      assert_equal(value2, @converter.convert(value1, unit1, unit2),
                    "line: v1=#{value1} u1=#{unit1} v2=#{value2} u2=#{unit2}")
       # from value2 to value1
-      assert_equal(value1, @interface.converter.convert(value2, unit2, unit1),
+      assert_equal(value1, @converter.convert(value2, unit2, unit1),
                    "line: v1=#{value2} u1=#{unit2} v2=#{value1} u2=#{unit1}")
     end
   end
