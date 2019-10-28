f529410f4daf97a2d5ea78c9371679a76c7e546e
youssefbenlemlih <youssefbenlemlih7@gmail.com>
2019-10-28 15:07:58 +0100

    added CodeMaker

---
+++ ptp4/code_maker.rb
@@ -0,0 +1,34 @@
+class CodeMaker
+  def initialize
+    @symbol_count = 6
+    @code_length = 4
+    @master_code = []
+  end
+
+  def master_code!(code)
+    @master_code = code
+  end
+
+  def evaluate(code)
+    hits = Hash.new(0)
+    code.each_with_index do |bit, i|
+      hits[:white] += 1 if white_hit?(bit, i)
+      hits[:black] += 1 if black_hit?(bit, i)
+    end
+    hits
+  end
+
+  private
+
+  def white_hit?(bit, pos)
+    if !black_hit?(bit, pos)
+      @master_code.include?(bit)
+    else
+      false
+    end
+  end
+
+  def black_hit?(bit, pos)
+    @master_code[pos] == bit
+  end
+end
