d9025e60496c23656b6870c4000a2ffb1aa4565d
youssef <youssefbenlemlih7@gmail.com>
2019-11-18 00:02:42 +0100

    documented Queue

--- ptp5/queue.rb
+++ ptp5/queue.rb
@@ -8,7 +8,7 @@ class QueueEnqueueError < StandardError; end
 class QueueDequeueError < StandardError; end
 
 # A simple Queue where elements are added to the top and
-# we only get the element that lastet the longest time within the Queue
+# only the last one can be retrieved (First in last out)
 class Queue
   attr_reader :content
   protected :content
@@ -17,6 +17,9 @@ class Queue
     @content = []
   end
 
+  # Adds an element to the queue
+  # Throws a QueueEnqueueError id the element is nil
+  # @param elem: The element to add
   def enqueue(elem)
     raise(QueueEnqueueError, 'You cannot add nil to this Queue!') if elem.nil?
 
@@ -24,20 +27,29 @@ class Queue
     elem
   end
 
+ # Adds an element to the queue
+ # Throws a QueueEnqueueError id the element is nil
+ # @param elem: The element to add
   def <<(elem)
     enqueue(elem)
   end
 
+
+  # Pops out the last element in the queue
+  # Throws a QueueEnqueueError id the queue is not empty
+  # @param elem: The element to add
   def dequeue
     raise(QueueDequeueError, 'The Queue is empty, you cannot dequeue anything of it!') if empty?
 
     @content.shift
   end
 
+  # Wether the Queue is empty (contains no element)
   def empty?
     @content.empty?
   end
 
+  # Wether both Queues contain the same elements
   def ==(other)
     return false if self.class != other.class
 
