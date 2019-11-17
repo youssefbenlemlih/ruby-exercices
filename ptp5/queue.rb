eed0acf98e1845de44b1a3fc37b31725c7e3fdfe
youssef <youssefbenlemlih7@gmail.com>
2019-11-18 00:26:27 +0100

    documented Queue and Stack

--- ptp5/queue.rb
+++ ptp5/queue.rb
@@ -8,7 +8,7 @@ class QueueEnqueueError < StandardError; end
 class QueueDequeueError < StandardError; end
 
 # A simple Queue where elements are added to the top and
-# only the last one can be retrieved (First in last out)
+# we only get the element that lasted the longest time within the Queue
 class Queue
   attr_reader :content
   protected :content
@@ -17,7 +17,7 @@ class Queue
     @content = []
   end
 
-  # Adds an element to the queue
+  # Adds an element on the top of the queue
   # Throws a QueueEnqueueError id the element is nil
   # @param elem: The element to add
   def enqueue(elem)
@@ -27,9 +27,9 @@ class Queue
     elem
   end
 
- # Adds an element to the queue
- # Throws a QueueEnqueueError id the element is nil
- # @param elem: The element to add
+  # Adds an element on the top of the queue
+  # Throws a QueueEnqueueError id the element is nil
+  # @param elem: The element to add
   def <<(elem)
     enqueue(elem)
   end
@@ -37,19 +37,18 @@ class Queue
 
   # Pops out the last element in the queue
   # Throws a QueueEnqueueError id the queue is not empty
-  # @param elem: The element to add
   def dequeue
     raise(QueueDequeueError, 'The Queue is empty, you cannot dequeue anything of it!') if empty?
 
     @content.shift
   end
 
-  # Wether the Queue is empty (contains no element)
+  # Whether the Queue is empty (contains no element)
   def empty?
     @content.empty?
   end
 
-  # Wether both Queues contain the same elements
+  # Whether the Queues are equal (contains the same elements)
   def ==(other)
     return false if self.class != other.class
 
@@ -60,6 +59,7 @@ class Queue
     @content.hash
   end
 
+  # String representation of the Queue
   def to_s
     @content.to_s
   end
--- ptp5/stack.rb
+++ ptp5/stack.rb
@@ -17,6 +17,9 @@ class Stack
     @content = []
   end
 
+  # Adds an element to the Stack
+  # Throws a StackPushError if the element is nil
+  # @param elem: The element to add
   def push(elem)
     raise(StackPushError, 'You cannot add nil to this Stack!') if elem.nil?
 
@@ -24,33 +27,43 @@ class Stack
     elem
   end
 
+  # Adds an element to the Stack
+  # Throws a StackPushError id the element is nil
+  # @param elem: The element to add
   def <<(elem)
     push(elem)
   end
 
+  # Pops out the last element in the Stack
+  # Throws a StackPopError if the Stack is not empty
   def pop
     raise(StackPopError, 'The Stack is empty, you cannot pop anything of it!') if empty?
 
     @content.delete_at(0)
   end
 
+  # Whether the Stack is empty (contains no element)
   def empty?
     @content.empty?
   end
 
+  # Whether the Stacks are equal (contain the same elements)
   def ==(other)
     return false if self.class != other.class
 
     @content == other.content
   end
 
+  # Needed method to use a Stack as a Key in a Hash
   def hash
     @content.hash
   end
 
+  # String representation of the Stack
   def to_s
     @content.to_s
   end
 
+  # Whether the Stacks are equal (contain the same elements)
   alias eql? ==
 end
