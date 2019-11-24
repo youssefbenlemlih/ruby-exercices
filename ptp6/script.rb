5568711eabd2f21079e3af13e83c24e684f6b6c8
youssef <youssefbenlemlih7@gmail.com>
2019-11-24 15:30:57 +0100

    corrected renamed methods in script.rb

--- ptp6/script.rb
+++ ptp6/script.rb
@@ -5,21 +5,25 @@ require_relative 'cat'
 require_relative 'dog'
 require_relative 'person'
 
-hans = Person.new('hans')
-joe = Person.new('joe')
+begin
+  hans = Person.new('hans')
+  joe = Person.new('joe')
+  bello = Dog.new('bello', Date.new(2019, 11, 19), hans)
+  mike = Dog.new('mike', Date.new(2010, 11, 1), hans)
+  tom = Cat.new("Tom", Date.new(2015, 02, 14), hans, joe)
+  tom2 = Cat.new("Tom", Date.new(2015, 02, 14), hans, joe)
+  jerry = Cat.new("Jerry", Date.new(2015, 02, 14), hans, joe)
 
-bello = Dog.new('bello', Date.new(2019,11,19), hans)
-mike = Dog.new('mike', Date.new(2010,11,1), hans)
+  tom.kill(mike)
+  tom.kill(jerry)
+  tom.kill(jerry)
+  mike.kill(bello)
+  mike.kill(tom)
+rescue StandardError => e
+  puts e.backtrace
+  puts e.message
+end
 
-tom = Cat.new("Tom", Date.new(2015,02,14), hans, joe)
-tom2 = Cat.new("Tom", Date.new(2015,02,14), hans, joe)
-jerry = Cat.new("Jerry", Date.new(2015,02,14), hans, joe)
-
-tom.kill(mike)
-tom.kill(jerry)
-tom.kill(jerry)
-mike.kill(bello)
-mike.kill(tom)
 puts hans
 puts joe
 puts bello
