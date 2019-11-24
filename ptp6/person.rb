29899ae568bb995a26f686090a6bada009757fe9
youssef <youssefbenlemlih7@gmail.com>
2019-11-24 15:55:42 +0100

    code quality

--- ptp6/person.rb
+++ ptp6/person.rb
@@ -1,8 +1,12 @@
 # Author:: Youssef Benlemlih
 # Author:: Jonas Krukenberg
 require_relative 'consistency'
+
+# A subclass of StandardError to manage not allowed Person operations
 class PersonError < StandardError;
 end
+# The class of Person
+# A person has a name and pets
 class Person
   include Consistency
   attr_reader :name
@@ -12,10 +16,14 @@ class Person
     @pets = []
   end
 
+  # Adds a pet to the person
   def add_pet(pet)
     @pets << pet
   end
 
+  # gives a service to the given Pet
+  # @param [Pet] pet the pet to receive the service
+  # @param [Symbol] service Either :feed of :stroke
   def give_service(pet, service)
     raise PersonError, 'A person can only stroke or feed a Pet' unless pet.is_a?(Pet)
 
@@ -23,7 +31,7 @@ class Person
   end
 
   def to_s
-    "#{self.class}: name: #{@name} pets: #{@pets.map { |pet| pet.name }}"
+    "#{self.class}: name: #{@name} pets: #{@pets.map(&:name)}"
   end
 
 end
--- ptp6/pet.rb
+++ ptp6/pet.rb
@@ -1,8 +1,13 @@
 # Author:: Youssef Benlemlih
 # Author:: Jonas Krukenberg
 require_relative 'consistency'
+
+# A subclass of [StandardError] to manage not allowed pet operations
 class PetError < StandardError
 end
+
+# The class of Pets
+# A pet has a a name, a birthday, and a number of lifes
 class Pet
   include Consistency
   attr_reader :name, :alive
@@ -15,30 +20,41 @@ class Pet
     @alive = true
   end
 
+  # Requests a service from the given Person
+  # @param [Person] person The person who will give the service
+  # @param [Symbol] service Either :feed of :stroke
   def request_service(person, service)
-    raise PetError, "A pet can only request a service from a Person" unless person.is_a?(Person)
+    raise PetError, 'A pet can only request a service from a Person' unless person.is_a?(Person)
 
     person.give_service(self, service)
   end
 
+  # Receives the service from the given Person
+  # @param [Person] giver the Person who gives the service
+  # @param [Symbol] service Either :feed of :stroke
   def get_service(giver, service)
     unless giver.is_a?(Person)
       raise PetError, 'A pet can only get a service from a Person'
     end
+
     if service == :stroke
       puts "I (#{name}) am getting stroked by #{giver.name}"
     elsif service == :feed
       puts "I (#{name}) am getting fed by #{giver.name}"
     else
-      raise PetError, "A Pet cannot recieve the service '#{service}'"
+      raise PetError, "A Pet cannot receive the service '#{service}'"
     end
   end
 
   def to_s
-    "#{self.class}: name: #{@name}, birthday: #{@birthday}, lifes: #{@lifes}, alive: #{@alive} #{self.special_to_s}"
+    "#{self.class}: name: #{@name}, birthday: #{@birthday}, lifes: #{@lifes}, alive: #{@alive} #{special_to_s}"
   end
 
-# @param [Pet] other The victim
+  def special_to_s
+    ''
+  end
+
+  # @param [Pet] other The victim
   def kill(other)
     if !is_a?(Cat) && other.is_a?(Cat)
       raise CatError, 'Only a cat can kill another Cat.'
@@ -55,6 +71,8 @@ class Pet
 
   protected
 
+  # Reduces the lifes by one
+  #  Checks if @alive
   def die
     @lifes -= 1
     @alive = @lifes != 0
