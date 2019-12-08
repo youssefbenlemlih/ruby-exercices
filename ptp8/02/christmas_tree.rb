3b71a7ff75273435813ddc39848160316bfe709a
kikelkik <j.krukenberg@gmx.de>
2019-12-08 20:24:50 +0100

    code quality

--- ptp8/02/christmas_tree.rb
+++ ptp8/02/christmas_tree.rb
@@ -1,42 +1,45 @@
 # Author:: Youssef Benlemlih
 # Author:: Jonas Krukenberg
 class ChristmasTree
+  # @param [Integer] n
   def initialize(n)
+    raise ArgumentError, 'n needs to be an Integer!' unless n.is_a?(Numeric) && n.integer?
+
     @n = n
   end
 
+  # print the christmas tree pattern well formatted to the console
   def draw_tree
     result = ''
     max_cols = @n + 1
     calc_tree.each do |row|
-      print = ' ' * ((max_cols - row.length) / 2)
-      print *= (@n + 1)
+      print = ' ' * ((max_cols - row.length) / 2) * max_cols
       print << row.join(' ')
       result << "#{print}\n"
     end
     puts result
   end
 
+  # generate a multidimensional array
+  # 1st layer: rows
+  # 2nd layer: elements
+  # @return [Array]
   def calc_tree
     tree = []
     n = 1
     while n <= @n
       result = []
-      if n == 1
-        result = [%w[0 1]]
-      else
-        tree.each do |row|
-          line1 = []
-          line2 = []
-          r = row.count
-          row.each_with_index { |elem, i|
-            line1 << "#{elem}0" if i > 0
-            line2 << "#{elem}0" if i == 0
-            line2 << "#{elem}1"
-          }
-          result << line1 unless r == 1
-          result << line2
+      result = [[0, 1]] if n == 1
+      tree.each do |row|
+        line1 = []
+        line2 = []
+        row.each_with_index do |elem, i|
+          line1 << "#{elem}0" if i.positive?
+          line2 << "#{elem}0" if i.zero?
+          line2 << "#{elem}1"
         end
+        result << line1 unless row.count == 1
+        result << line2
       end
       tree = result
       n += 1
