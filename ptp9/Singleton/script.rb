3ec006ef69773fb79a5626dea22cde1e3aa226c8
youssef <youssefbenlemlih7@gmail.com>
2019-12-15 13:48:28 +0100

    added some singleton code

---
+++ ptp9/Singleton/script.rb
@@ -0,0 +1,47 @@
+require('singleton')
+
+class Clazz
+  attr_reader :name
+  def Clazz.singleton_method_added(id)
+    puts "Adding #{id.id2name} to Clazz"
+  end
+  def initialize(name)
+    @name = name
+  end
+  def self.third_singleton_method
+    puts 'third_singleton_method'
+  end
+end
+
+class ClazzSingelton include Singleton
+  attr_reader :name
+end
+
+a = Clazz.new('tom')
+a2 = Clazz.new('tom2')
+a.define_singleton_method(:my_singlton_method) do
+  puts 'the singleton method'
+end
+def Clazz.second_singleton_method 
+  puts "second singleton method"
+end
+b = a.singleton_class
+c = a.singleton_class
+singleton = ClazzSingelton.instance()
+singleton2 = ClazzSingelton.instance()
+puts singleton
+puts singleton2
+puts a.inspect
+puts b.inspect
+puts c.inspect
+puts c.name
+puts a==b
+puts c==b
+a.my_singlton_method
+# a2.my_singlton_method # doesnt work
+#b.my_singlton_method # doesnt work
+
+#basicObject = BasicObject.new
+#puts "basicObject=>#{basicObject.class}"
+#basicObjectSingleton = basicObject.singleton_class
+#puts "basicObjectSingleton=>#{basicObjectSingleton.class}"
